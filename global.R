91fed4f57e8ddc72080a1e101023256e7ae9979c
jrf1111 <39165225+jrf1111@users.noreply.github.com>
2020-03-26 16:56:02 -0500

    Comment out install.packages

--- global.R
+++ global.R
@@ -3,8 +3,8 @@ libs = c("shiny", "tidyverse", "plotly",
 				 "viridis", "stringr", "ggplot2", "readr", "DT")
 
 for(p in libs){
-	if(!require(p, character.only = TRUE)) 
-		install.packages(p)
+	# if(!require(p, character.only = TRUE)) 
+	# 	install.packages(p)
 	library(p, character.only = TRUE)
 }
 
