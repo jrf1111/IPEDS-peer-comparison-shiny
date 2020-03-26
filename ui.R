050612468f028078c790b37b66ec68ef0e0b05cc
jrf1111 <39165225+jrf1111@users.noreply.github.com>
2020-03-26 16:38:36 -0500

    Delete old code

--- ui.R
+++ ui.R
@@ -3,13 +3,8 @@ navbarPage("IPEDS Peer Comparison Dashboard",
 					 tabPanel("All Data",
 					 				 downloadButton('downloadData', 'Download'),
 					 				 DT::dataTableOutput("table1")), 
-					 tabPanel("Approvals vs Declines",
-					 				 sidebarLayout(sidebarPanel(
-					 				 	helpText("The total number of approvals and denials in the following graph, we realize that since 2015, approvals are dwindling.", 
-					 				 					 "On the other hand, denials are surging.")),
-					 				 	mainPanel(plotlyOutput("plot1"))
-					 				 )),
-					 tabPanel("Geographic",
+					 
+					 tabPanel("Dashboard",
 					 				 sidebarLayout(sidebarPanel(
 					 				 	selectInput('x', 'Variable', sort(unique(mdata$Question))),
 					 				 	sliderInput("size", "Text size", min = 6, max = 50, value = 16, step = 1, round = T)
